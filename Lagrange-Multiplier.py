6baa6640a8bca7f3c1f7e0f93af279599f86ef2c
Muhammet Ali Kaya <77796177+MAli-Kaya@users.noreply.github.com>
2021-06-18 19:28:59 +0300

    Genel Düzenlemeler

--- Lagrange-Multiplier.py
+++ Lagrange-Multiplier.py
@@ -1,23 +1,17 @@
-
 from sympy import *
+x, y, λ = symbols("x y λ")
+"""
+Algoritmanın çalışması için f ve g fonksiyonlarının girilmesi yeterlidir.
+f için f(x,y)
+g için g(x,y) kısıt fonksiyonu
+
+video örneği:
+f = x**2 - 2*x + y**2 - y + 1.25
+g = x**2 + y**2 - 5
 
 """
-f = (x**2 + 1) / (x - 1)
 
-print(f.subs(x,2))
-
-pprint(1 + sqrt(2))
-pprint(Integral(sqrt(1/x) + exp(x),x))
-
-print_latex(Integral(sqrt(1/x) + exp(x),x))
-
-f = sin(x)**2 + cos(x)**2
-pprint(f)
-print_latex(f)
-
-f = x**2 - 2*x + y**2 - y + 1.25
-g = x**2 + y**2 - 5 """
-x, y, λ = symbols("x y λ")
+#Final Sorusu
 f = x**2 + 4*y**2 - 2*x + 8*y
 g = x + 2*y - 7
 
@@ -27,27 +21,29 @@ def lagrangeFonk(f,g):
     print("\nLagrange Formülü:\nL = f(x,y) + λ*g(x,y)\n")
     print("Lagrange Denklemi:")
     pprint(L)
-    print("")
-    # pprint(collect(L.expand(),x))
-    # pprint(simplify(L))
-    print("")
-    # pprint(L.factor())
+    print("\n-------------------------------------------")
     return L
-
-L = lagrangeFonk(f,g)
 
 def turev(L, a):
     La = diff(L,a)
-    print("L'nin {}'e göre türevi:\nL{}(x, y, λ) = ".format(a,a),end="")
+    print("\nL'nin {}'e göre türevi:".format(a))
+    pprint(Derivative(L, a))
+    #print("LATEX Kodu: ",end="")
+    #print_latex(Derivative(L, a))
+    print("\nL{}(x, y, λ) = ".format(a),end="")
     pprint(La)
+    #print("LATEX Kodu: ",end="")
+    #print_latex(La)
     return La
 
 def denklemYaz(L,a):
     print("ise {} = ".format(a))
     eqx = solveset(Eq(L, 0), a)
+    #eqx = str(eqx)
     pprint(eqx)
-    print("")
-
+    #print("LATEX Kodu: ",end="")
+    #print_latex(eqx)
+    print("-------------------------------------------")
 
 def optimumNokta(x, y, λ, L):
     # x için türev
@@ -64,21 +60,18 @@ def optimumNokta(x, y, λ, L):
     x1 = liste[0][0]
     y1 = liste[0][1]
     # x ve y değerlerini ve optimum noktayı yazdırma
+    print("-------------------------------------------")
     pprint("\nise x = {}, y = {} olarak bulunur.".format(x1, y1))
     print("\nBuradan F(x,y) için optimum nokta ({}, {}) olur.".format(x1, y1))
-
     return x1,y1
-
-opNokta = optimumNokta(x, y, λ, L)
-
-x1 = opNokta[0]
-x2 = opNokta[1]
 
 def sonuc(x1, y1, x, y,f):
     # foksiyon değerinin adım adım yazdırılması
     print("\nF({}, {}) fonksiyonunun hesabı:".format(x1, y1), )
     # 1. adım fonksiyonun kendisi
     pprint(f)
+    # print("LATEX Kodu: ",end="")
+    # print_latex(f)
     # önce değişkenleri string e çevirmeliyiz ki fonksiyonu manipüle edebilelim
     strf = str(f)
     strX = str(x1)
@@ -89,8 +82,13 @@ def sonuc(x1, y1, x, y,f):
     strf = strf.replace("x", strX)
     strf = strf.replace("y", strY)
     strf = strf.replace("**", "^")
+    strf = strf.replace("*", "⋅")
     print("\n= {}\n".format(strf))
     # 3. adım sonucu yazdırma
     print("=",f.subs(x, x1).subs(y, y1))
 
-FoksiyonDeğeri = sonuc(x1, x2, x, y, f)
+L = lagrangeFonk(f,g)
+opNokta = optimumNokta(x, y, λ, L)
+x1 = opNokta[0]
+x2 = opNokta[1]
+FoksiyonDegeri = sonuc(x1, x2, x, y, f)
