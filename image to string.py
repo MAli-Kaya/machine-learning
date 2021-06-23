acfc4631c87f71c22b0036419e5f07aa72ffde3b
Muhammet Ali Kaya <77796177+MAli-Kaya@users.noreply.github.com>
2021-06-23 09:54:50 +0300

    image to string

--- image to string.py
+++ image to string.py
@@ -4,6 +4,6 @@ except ImportError:
     import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Muhammet Ali Kaya\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-img = pytesseract.image_to_string(Image.open('veri.png'), lang='tur')
-x = img.splitlines()
+img = pytesseract.image_to_string(Image.open('veri.jpeg'), lang='tur')
+x = img.split()
 print("Okunan Veri:\n",x,"\n")
